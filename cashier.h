d187e324911750bfd0ac3f5a8255c923492cc87b
KrystianRucki <>
2025-01-18 01:08:12 +0100

    Starting again, deleting all files, trying with a different approach locally - FIFO and messages

--- cashier.h
+++
@@ -1,8 +0,0 @@
-#include "globals.h"
-#include <stdbool.h>
-bool is_cashier_open();
-void process_passenger();
-void cashier_cycle();
-void create_cashier();
-void wait_cashier();
-
--- timemgmt.h
+++
@@ -1,5 +0,0 @@
-#include "globals.h"
-
-void start_time_manager(); //time manager for Tp Tk - organizacja wycieczek nie cala symulacja
-void create_time();
-void wait_time();
